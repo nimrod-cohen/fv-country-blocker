1ddd83f4ef8fec90be28d57da2d970f73364ccae
nimrod-cohen <nimrod.cohen@gmail.com>
2024-10-17 17:29:35 +0300

    allow caching of github responses

--- includes/github-updater.php
+++ includes/github-updater.php
@@ -40,7 +40,7 @@ if (!class_exists('GitHubPluginUpdater')) {
       $file = __FILE__;
       $this->plugin_slug = explode('/', plugin_basename($file))[0];
       $this->cache_key = $this->plugin_slug . '_transient_data';
-      $this->cache_allowed = false;
+      $this->cache_allowed = true;
       $this->get_plugin_details();
 
       add_filter('plugins_api', [$this, 'info'], 20, 3);
