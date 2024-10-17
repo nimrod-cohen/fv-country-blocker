099ecd5b4aa6419aab043b733428b5505146c1d4
nimrod-cohen <nimrod.cohen@gmail.com>
2024-10-17 19:05:15 +0300

    vbump

--- fv-country-blocker.php
+++ fv-country-blocker.php
@@ -3,7 +3,7 @@
  * Plugin Name: FV Country Blocker
  * Plugin URI: https://github.com/nimrod-cohen/fv-country-blocker
  * Description: Block visitors from specific countries using MaxMind GeoIP database.
- * Version: 1.0.16
+ * Version: 1.0.17
  * Author: nimrod-cohen
  * Author URI: https://github.com/nimrod-cohen/fv-country-blocker
  * License: GPL-2.0+
