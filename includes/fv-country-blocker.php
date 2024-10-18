86dc22835774ec3449130fcfefda8e26f7142bb7
nimrod-cohen <nimrod.cohen@gmail.com>
2024-10-18 18:10:31 +0300

    fix checker activation

--- includes/fv-country-blocker.php
+++ includes/fv-country-blocker.php
@@ -63,22 +63,8 @@ class FV_Country_Blocker {
   }
 
   public function check_version() {
-    if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-      $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'fv-country-blocker', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/nimrod-cohen/fv-country-blocker', // the GitHub API url of your GitHub repo
-        'raw_url' => 'https://raw.github.com//nimrod-cohen/fv-country-blocker/main', // the GitHub raw url of your GitHub repo
-        'github_url' => 'https://github.com/nimrod-cohen/fv-country-blocker', // the GitHub url of your GitHub repo
-        'zip_url' => 'https://github.com/nimrod-cohen/fv-country-blocker/zipball/main', // the zip url of the GitHub repo
-        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '6.0', // which version of WordPress does your plugin require?
-        'tested' => '6.6.2', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '' // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-      );
-      new GitHubPluginUpdater($config);
-    }
+    $updater = new GhPluginUpdater(__FILE__, 'nimrod-cohen', 'fv-country-blocker');
+    $updater->init();
   }
 
   public function register_settings() {
